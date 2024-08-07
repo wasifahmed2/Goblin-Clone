3b9ea76858ac24079e94ba5ab75b405f8497bd32
wasif <ahmedwasif036@gmail.com>
2024-08-08 02:11:22 +0500

    commit

--- script.js
+++ script.js
@@ -16,4 +16,4 @@ const sr = ScrollReveal({
 sr.reveal('.hero-text',{delay:200, origin: 'top'});
 sr.reveal('.hero-img',{delay:450, origin: 'top'});
 sr.reveal('.icons',{delay:500, origin: 'left'})
-sr.reveal('.scroll-down',{delay:500, origin: 'right'})
+sr.reveal('.scroll-down',{delay:500, origin: 'right'});
